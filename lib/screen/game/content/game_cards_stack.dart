f87195079de19ca9dd754b518f9d34f9d33fa841
iliia Germaniuk <iliia.germaniuk@gmail.com>
2023-02-02 14:03:39 -0600

    no message

--- lib/screen/game/content/game_cards_stack.dart
+++ lib/screen/game/content/game_cards_stack.dart
@@ -18,14 +18,6 @@ class GameCardsStack extends StatelessWidget {
           onStackFinished:
               () {
             // todo
-            // return const Center(
-            //     child: Text(
-            //   'карточки закончились',
-            //   style: TextStyle(
-            //       color: Colors.white,
-            //       fontWeight: FontWeight.bold,
-            //       fontSize: 30),
-            // ));
           },
           itemBuilder: (BuildContext context, int index) {
             return Card(
